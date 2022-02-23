4637ccc14411c9c31b6fa82414352e703d31a47c
Aeon Borealis <90811245+aeonborealis@users.noreply.github.com>
2022-02-23 12:37:15 -0800

    Update PHP in depth training.php

--- Programming Techniques/PHP techniques/PHP in depth training/PHP in depth training.php
+++ Programming Techniques/PHP techniques/PHP in depth training/PHP in depth training.php
@@ -221,6 +221,44 @@ echo 2 <=> 1; // 1
       }
   }
 
+// Functions 
+
+// A function is a block of statements that can be used repeatedly in a program.
+
+function say_hello() {
+  return "Hello!";
+}
+
+echo say_hello();
+
+// Simple Function + Parameter + call 
+
+function say_hello($friend) {
+  return "Hello " . $friend . "!";
+}
+
+echo say_hello('Tommy');
+
+// strtoupper - Makes all Chars BIGGER AND BIGGER!
+
+function makeItBIG($a_lot_of_names) {
+  foreach($a_lot_of_names as $the_simpsons) {
+    $BIG[] = strtoupper($the_simpsons);
+  }
+  return $BIG;
+}
+
+$a_lot_of_names = ['Homer', 'Marge', 'Bart', 'Maggy', 'Lisa'];
+var_dump(makeItBIG($a_lot_of_names));
+
+// PHP Array 
+
+
+
+
+
+
+
 
 
 
